159c52d59012d3037621dea0073423b4eb75efad
Victor <vitya898989@gmail.com>
2021-12-08 19:50:15 +0300

    sfsd

---
+++ form-action.php
@@ -0,0 +1,36 @@
+<?
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+
+$mail = new PHPMailer(true);
+$mail->CharSet = 'UTF-8';
+$mail->setLanguage('ru', 'phpmailer/language/');
+$mail->IsHTML(true);
+
+$mail->setFrom('jeniock@mail.ru');
+$mail->addAddress('vitya898989@gmail.com'); 
+$mail->Subject = 'Заявка на консультацию';
+        $body = '<html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+						  		<p>Имя: '.$_POST['name'].'</p>
+								<p>Фамилия: '.$_POST['surname'].'</p>
+								<p>Телефон: '.$_POST['tel'].'</p>
+                    </body>
+                </html>'; 
+        $mail->Body = $body;
+	if (!$mail->send()) {
+		$message = 'Ошибка';
+	} else {
+		$message = 'Данные отправлены';
+	}
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
--- js/script.js
+++ js/script.js
@@ -28,6 +28,25 @@ nav.onclick = function (e) {
 		nav.classList.remove('nav-open');
 	}
 };
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+	link.addEventListener('click', function (e) {
+		e.preventDefault();
+
+		let href = this.getAttribute('href').substring(1);
+
+		const scrollTarget = document.getElementById(href);
+
+		let topOffset;
+		topOffset = document.querySelector('.header__content').offsetHeight;
+		const elementPosition = scrollTarget.getBoundingClientRect().top;
+		const offsetPosition = elementPosition - topOffset;
+		window.scrollBy({
+			top: offsetPosition,
+			behavior: 'smooth'
+		});
+	});
+});;
 let mainButton = document.querySelector('.main__button');
 let mainVideo = document.querySelector('.main__video');
 
@@ -53,5 +72,86 @@ new Swiper('.reviews-slider', {
 		},
 	},
 });;
-;
+let telMask = document.querySelector('.tel-mask');
+let onlyLetter = document.querySelectorAll('.only-letter');
+
+function setCursorPosition(pos, elem) {
+	elem.focus();
+	if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+	else if (elem.createTextRange) {
+		var range = elem.createTextRange();
+		range.collapse(true);
+		range.moveEnd("character", pos);
+		range.moveStart("character", pos);
+		range.select()
+	}
+}
+function mask(event) {
+	var matrix = this.defaultValue,
+		i = 0,
+		def = matrix.replace(/\D/g, ""),
+		val = this.value.replace(/\D/g, "");
+	def.length >= val.length && (val = def);
+	matrix = matrix.replace(/[_\d]/g, function (a) {
+		return val.charAt(i++) || "_"
+	});
+	this.value = matrix;
+	i = matrix.lastIndexOf(val.substr(-1));
+	i < matrix.length && matrix != this.defaultValue ? i++ : i = matrix.indexOf("_");
+	setCursorPosition(i, this)
+}
+
+telMask.addEventListener("input", mask, false);
+
+for (let i = 0; i < onlyLetter.length; i++) {
+	onlyLetter[i].addEventListener('keyup', function () {
+		this.value = this.value.replace(/[\w]/g, '');
+	});
+}
+
+let form = document.querySelector('form');
+let nameInput = document.querySelector('.order__name');
+let surnameInput = document.querySelector('.order__surname');
+let telInput = document.querySelector('.order__tel');
+let orderMessage = document.querySelector('.order__message');
+
+form.onsubmit = async (e) => {
+	e.preventDefault();
+	let valid = true;
+	if (nameInput.value == '') {
+		valid = false;
+		nameInput.classList.add('error-input');
+	}
+	if (surnameInput.value == '') {
+		valid = false;
+		surnameInput.classList.add('error-input');
+	}
+	if (telInput.value == '+7(___)_______') {
+		valid = false;
+		telInput.classList.add('error-input');
+	}
+	if (valid) {
+		// let response = await fetch('form-action.php', {
+		// 	method: 'POST',
+		// 	body: new FormData(form)
+		// });
+		// if (response.ok) {
+		form.classList.remove('show');
+		orderMessage.classList.add('show');
+		form.reset();
+		// } else {
+		// 	alert('Произошла ошибка отправки, попробуйте еще раз!');
+		// }
+	}
+}
+
+nameInput.onfocus = function () {
+	nameInput.classList.remove('error-input');
+}
+surnameInput.onfocus = function () {
+	surnameInput.classList.remove('error-input');
+}
+telInput.onfocus = function () {
+	telInput.classList.remove('error-input');
+};
 
